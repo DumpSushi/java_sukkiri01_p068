86d7f8b05d681025f5a298cb040985549cb3ed58
DumpSushi <DumpSushi@users.noreply.github.com>
2016-07-28 17:44:25 +0900

    2-1

---
+++ src/p068/Main.java
@@ -0,0 +1,28 @@
+package p068;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		// 2-1
+		{
+			Date nowDate = new Date();
+			Calendar calcCalendar = Calendar.getInstance();
+			calcCalendar.setTime(nowDate);
+			int day = calcCalendar.get(Calendar.DAY_OF_MONTH);
+			day += 100;
+			calcCalendar.set(Calendar.DAY_OF_MONTH, day);
+			Date newDay = new Date();
+			newDay = calcCalendar.getTime();
+
+			SimpleDateFormat f = new SimpleDateFormat("西暦yyyy年/MM月/dd日 HH:mm:ss");
+			String s = f.format(newDay);
+			System.out.println(s);
+		}
+	}
+
+}
