dcd078845b62b4229bed924b1559753e77956fa5
DumpSushi <DumpSushi@users.noreply.github.com>
2016-07-29 10:54:18 +0900

    Java8のAPIでやる。

--- src/p068/Main.java
+++ src/p068/Main.java
@@ -1,6 +1,8 @@
 package p068;
 
 import java.text.SimpleDateFormat;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -21,6 +23,15 @@ public class Main {
 
 			SimpleDateFormat f = new SimpleDateFormat("西暦yyyy年/MM月/dd日 HH:mm:ss");
 			String s = f.format(newDay);
+			System.out.println(s);
+		}
+
+		// 2-2
+		{
+			ZonedDateTime nowDate = ZonedDateTime.now();
+			ZonedDateTime newDate = nowDate.plusDays(100);
+			DateTimeFormatter f = DateTimeFormatter.ofPattern("西暦yyyy年/MM月/dd日 HH時mm分ss秒");
+			String s = f.format(newDate);
 			System.out.println(s);
 		}
 	}
